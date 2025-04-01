293333210b15cde51f339fddc5b98bd9f0534934
lkingl123 <lokekingloong@gmail.com>
2025-04-01 13:29:48 -0600

    updated config values

--- src/lib/authConfig.ts
+++ src/lib/authConfig.ts
@@ -1,8 +1,8 @@
 export const msalConfig = {
     auth: {
-      clientId: "b1f9b34c-2827-4b05-96a6-ef1f54728ce0",
+      clientId: "ff623be8-fc5b-4348-965b-4cd2a59103ee",
       authority: "https://login.microsoftonline.com/a68f6642-b44d-47f5-b59a-bb5106142d86",
-      redirectUri: "/",
+      redirectUri: "https://chem-lab-portal.vercel.app/",
     },
   };
   
