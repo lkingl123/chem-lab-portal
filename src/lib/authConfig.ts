4df3d471f8f9de6ff05308310a3c4fd02023bc1f
lkingl123 <lokekingloong@gmail.com>
2025-04-01 12:08:33 -0600

    putting authConfig values in the commit for now

--- src/lib/authConfig.ts
+++ src/lib/authConfig.ts
@@ -1,7 +1,7 @@
 export const msalConfig = {
     auth: {
-      clientId: "YOUR_CLIENT_ID",
-      authority: "https://login.microsoftonline.com/YOUR_TENANT_ID",
+      clientId: "b1f9b34c-2827-4b05-96a6-ef1f54728ce0",
+      authority: "https://login.microsoftonline.com/a68f6642-b44d-47f5-b59a-bb5106142d86",
       redirectUri: "/",
     },
   };
