96c8e60f2ff19d43acb4f6ef11961ec1c9995927
lkingl123 <lokekingloong@gmail.com>
2025-04-10 15:05:29 -0600

    feat: update Tailwind CSS configuration and dependencies, add PostCSS and Autoprefixer

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      './src/**/*.{js,ts,jsx,tsx}',
+      './app/**/*.{js,ts,jsx,tsx}',
+    ],
+    darkMode: 'media', // 👈 uses Mac's system theme setting
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
